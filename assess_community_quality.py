64974abf49f434d6b68e84016db25126ead1ad74
Ben Chamberlain <ben.chamberlain@asos.com>
2017-03-29 14:29:43 +0100

    removed unused functions and imports

--- assess_community_quality.py
+++ assess_community_quality.py
@@ -7,51 +7,25 @@ author: Ben Chamberlain 26/1/2014
 """
 
 from __future__ import division
-import os, csv
+import csv
 import numpy as np
 
 np.seterr(all='warn')  # To show floating point errors
-import cPickle, csv
-from time import gmtime, strftime, sleep
-from IPython import embed
 import pandas as pd
-import numpy.ma as ma
 import networkx as nx
-from itertools import combinations
 from datetime import datetime
-from influencer_index.src import star_index
-
-# from science_production.utilities_python import string_to_index
-
-# from science_production.utilities_python import string_to_index
-# from science_sandbox.locality_sensitive_hashing.PR_community_detection import calculate_PR,get_cut
-
-# Inputs
-package_directory = os.path.dirname(os.path.abspath(__file__))
-TAG_FILE_PATH = "resources/all_handles_tag3.csv"
-
-# matrix_npz = os.environ['GIT_FOLDER'] + "/science_production/large_resources/community_exploration/matrix.npz"
-# star_index_pickle = os.environ['GIT_FOLDER'] + "/science_production/community_exploration/resources/star_index.pickle"
-# processed_matrix = "matrix_proportion.npz"
-# matrix_ready = os.environ['GIT_FOLDER'] + "/science_production/large_resources/community_exploration/matrix.npz"
-
-# load the twitter data into memory
-twit_star_compression = star_index.StarCompression(network='twitter', filter='influencer')
-star_lookup = twit_star_compression.lookup
-max_int = twit_star_compression.max_int
-twit_star_compression.load_signatures()
-signatures = twit_star_compression.get_signatures()
 
 
 def calculate_PR(teleport_set, A, beta=0.85, n_iterations=2):
     """
     calculate the PageRank vector
-    beta - the teleport probability, in this case how much probability goes back
+    :param beta: - the teleport probability, in this case how much probability goes back
     to the seed at each iteration
-    teleport set - a randomly chosed vertex
-    A - the adjacency matrix for the community
-    n_iterations - the number of iterations of PR to run, more is more accurate, 
+    :param teleport_set: - a randomly chosed vertex
+    :param A: the adjacency matrix for the community
+    :param n_iterations: The number of iterations of PR to run, more is more accurate,
     but in practice two is adequate
+    :return A numpy array PageRank vector
     """
     # Create seed and label strength vectors
     R = np.ones((A.shape[0], 1), dtype=np.float16)  # Label strength vector
@@ -73,6 +47,10 @@ def get_cut(PR_vector, degree_vector, A):
     A Sweep is a commonly used method of extracting a graph cut from a vector.
     Sweeps through the PageRank vector repeatedly recalculating conductance
     and storing the minimum found
+    :param PR_vector: The PageRank of each vertex
+    :param degree_vector: The degree of each vertex
+    :param A: The adjacency matrix
+    :return: The minimum value of the conductance found
     """
     # get a degree normalized PR vector
     dn_vector = np.divide(PR_vector, degree_vector)
@@ -99,21 +77,6 @@ def get_cut(PR_vector, degree_vector, A):
     return min_conductance
 
 
-def get_community_ids(tag):
-    """
-    extracts all ids of stars who's tag is tag
-    """
-    # Load tags
-    tags = pd.io.parsers.read_csv(TAG_FILE_PATH)
-    tags = tags.drop_duplicates('Handle')  # Remove duplicates
-    tags = tags.set_index('Handle')  # Make handle the index
-
-    community = tags[tags['Tag'].str.contains(tag, na=False, case=False)]
-
-    ids = community['NetworkID']
-    return ids.tolist()
-
-
 def create_graphml(A, communities, handles, output, threshold=0.0):
     """
     Produces a graph ml file suitable for visualising in Gephi
@@ -124,11 +87,9 @@ def create_graphml(A, communities, handles, output, threshold=0.0):
     :param handles: The handles of the community members
     :param output:
     :param threshold:
-    :return:
+    :return: None
     """
     g = nx.Graph()
-
-    community_size, n_star = A.shape
 
     internal_edges = A[:, communities]
 
@@ -138,84 +99,51 @@ def create_graphml(A, communities, handles, output, threshold=0.0):
             if out_idx < idx:
                 if edge > threshold:
                     g.add_edge(out_idx, idx, weight=float(edge))
-                    # print float(edge)
 
     nx.write_graphml(g, output)
 
 
-def get_jaccards(signatures, star_lookup, indices, just_stars=True):
+def get_jaccards(community, all_data):
     """
     a vectorised one versus all Jaccard calculation
-    :param signatures: the matrix of minhash signatures for all influencers
-    :param indices: The indices into the signatures matrix for the community members
-    :param just_stars: If true, only compare with other stars rather than all influencers. This need to be true unless
-    you have 32GB+ of RAM or memory will get blown
+    :param community: A pandas DataFrame containing the minhashes for a single community
+    :param all_data: A pandas DataFrame containing the minhashes of the dataset
+    :return A numpy array of minhash Jaccard estimates of shape(community.shape[0], all_data.shape[0])
     """
-    try:
-        community_size = len(indices)
-    except:
-        community_size = 1
+    tag, df = community
+    community_size, n_hashes = df.shape
+    universe_size, _ = all_data.shape
 
-    n_stars, n_hashes = signatures.shape
+    # if just_stars:
+    #     lookup_df = star_lookup.df
+    #     star_lookup = lookup_df[lookup_df["isstar"] == True]
+    #     star_indices = star_lookup.index.values
+    #     # get rid of indices that were added after the last minhashes were generated
+    #     star_indices = star_indices[star_indices < signatures.shape[0]]
+    #     n_stars = len(star_indices)
+    #     star_signatures = signatures[star_indices, :]
 
-    if just_stars:
-        lookup_df = star_lookup.df
-        star_lookup = lookup_df[lookup_df["isstar"] == True]
-        star_indices = star_lookup.index.values
-        # get rid of indices that were added after the last minhashes were generated
-        star_indices = star_indices[star_indices < signatures.shape[0]]
-        n_stars = len(star_indices)
-        star_signatures = signatures[star_indices, :]
+    print 'calculating jaccard coefficients for', community_size, 'community members against universe of ', universe_size, ' members'
 
-    print 'calculating jaccard coefficients for', community_size, 'community members against ', n_stars, ' stars'
+    jaccards = np.zeros((community_size, universe_size))
+    community_sigs = df.values
+    all_sigs = all_data.values
+    for idx in range(community_size):
+        comparison_signature = community_sigs[idx, :]
 
-    jaccards = np.zeros((community_size, n_stars))
-
-    for jacc_idx, star in enumerate(indices):
-        comparison_signature = signatures[star, :]
-
-        assert comparison_signature[0] != np.iinfo(
-            np.uint64).max, "attempting to calculate the Jaccard of a star that we don't have data for"
+        # assert comparison_signature[0] != np.iinfo(
+        #     np.uint64).max, "attempting to calculate the Jaccard of a star that we don't have data for"
 
         # tile for broadcasting
-        tiled_community = np.tile(comparison_signature, (n_stars, 1))
+        tiled_community = np.tile(comparison_signature, (universe_size, 1))
         # do a vectorize element-wise star1 == star_j for all j
-        if just_stars:
-            collisions = star_signatures == tiled_community
-        else:
-            collisions = signatures == tiled_community
-
-        jacc = np.sum(collisions, 1) / n_hashes
-
-        jaccards[jacc_idx, :] = jacc
-
-        print 'community member ', jacc_idx, ' complete'
+        collisions = all_sigs == tiled_community
+        jacc = np.sum(collisions, axis=1) / float(n_hashes)
+        jaccards[idx, :] = jacc
+        if idx % 100 == 0:
+            print 'community member ', idx, ' complete'
 
     return jaccards
-
-
-def get_jaccards_vec(signatures, idx):
-    """
-    a very vectorised one versus all Jaccard calculation
-    overflows memory for large communities though
-    """
-    try:
-        community_size = len(idx)
-    except:
-        community_size = 1
-    n_stars, n_hashes = signatures.shape
-
-    comparison_signatures = signatures[idx, :]
-    # reshape for broadcasting
-    comparison_signatures = comparison_signatures.reshape((community_size, 1, n_hashes))
-    # do a vectorize element-wise star1 == star_j for all j
-    tiled_sigs = np.tile(signatures, (community_size, 1, 1))
-
-    tiled_community = np.tile(comparison_signatures, (1, n_stars, 1))
-
-    collisions = tiled_sigs == tiled_community
-
-    return np.sum(collisions, 2) / n_hashes
 
 
 def get_node_list(node_file, col_idx=0, has_header=True):
@@ -233,7 +161,7 @@ def get_node_list(node_file, col_idx=0, has_header=True):
     return retval
 
 
-def get_internal_weight(internal_edges, community_size):
+def get_internal_weight(internal_edges):
     """
     calculates the weight of internal edges
     internal_edges - the edges weights inside the community
@@ -257,7 +185,7 @@ def get_external_weight(A, internal_edge_weight):
     return external_edge_weight
 
 
-def calculate_seperability(m_s, c_s):
+def calculate_separability(m_s, c_s):
     """
     m_s is the total weight of edges between two nodes within the community
     c_s is the total weight of edges where just one node is in the community
@@ -350,15 +278,6 @@ def calculate_clustering_coefficient(internal_edges):
     return np.mean(clustering_coefficients)
 
 
-def output_analysis(out_file, tag, clustering_coefficient, cohesiveness, density, seperability):
-    """
-    write out the results to file
-    """
-    full_path = out_file + '/' + tag + '.csv'
-    with open(full_path, 'wb') as f:
-        writer = csv.writer(f)
-
-
 def change_indices(indices, star_lookup):
     """
     switch from influencer indices to star indices
@@ -379,51 +298,28 @@ def change_indices(indices, star_lookup):
     return star_indices
 
 
-def filter_none_stars(ids, star_lookup):
-    """
-    removes non-stars
-    :param ids:
-    :param star_lookup:
-    :return:
-    """
-    df = star_lookup.df
-    stars = df[df["isstar"] == True]
-    star_ids = stars['id'][stars['id'].isin(ids)]
-    return star_ids
-
-
-def run_analysis_suite(ids, signatures, star_lookup, generate_graphml=False, tag=None):
+def run_analysis_suite(community, data, generate_graphml=False):
     """ 
     runs the full suite of community analysis metrics for a single community
     """
-    assert len(ids) > 1, "communities are only defined for two or more nodes"
     # set the number of restarts for cohesiveness - each restart tries to find a different sub-community
     n_iterations = 10
-    # make sure they are all stars
-    print 'number of ids', len(ids)
-    ids = filter_none_stars(ids, star_lookup)
-    print 'number of stars', len(ids)
-    # get the indices into our Jaccard matrix / minhash signatures
-    indices = np.array(star_lookup.id(ids)['index'])
-    # remove any stars that have been added to the index since the minhashes were last updates
-    indices = indices[indices < signatures.shape[0]]
-    # remove any indices that we don't have stars for - these indices have all signatures set to max int
-    good_stars = signatures[indices, 0] != max_int
-    indices = indices[good_stars]
+
     # get the community size, this may not be the same as the number of ids as not all ids are indexed
-    indices = list(indices)
-    community_size = len(indices)
+    community_size = data.shape[0]
     # extract all weighted edges for the community
 
-    jaccs = get_jaccards(signatures, star_lookup, indices)
-    handles = star_lookup.index(indices)['handle']
+    jaccs = get_jaccards(community, data)
+    indices = community.index.values
+    # handles = star_lookup.index(indices)['handle']
     # switch from influencer to star indices
-    indices = change_indices(indices, star_lookup)
+    # indices = change_indices(indices, star_lookup)
     if generate_graphml:
         create_graphml(jaccs, indices, handles,
                        'local_results/' + tag + str(datetime.now().strftime("%Y%m%d-%H%M%S")) + '.graphml')
     # get just internal edges
     internal_edges = jaccs[:, indices]
+    assert np.trace(internal_edges) == community_size, 'error generating jaccards - all self jaccards should be 1'
     # remove the self loops
     np.fill_diagonal(internal_edges, 0)
     # calculate some of the parameters of the metrics
@@ -433,85 +329,32 @@ def run_analysis_suite(ids, signatures, star_lookup, generate_graphml=False, tag
     clustering_coefficient = calculate_clustering_coefficient(internal_edges)
     cohesiveness = calculate_cohesiveness(internal_edges, n_iterations, community_size)
     density = calculate_density(internal_weight, community_size)
-    seperability = calculate_seperability(internal_weight, external_weight)
+    separability = calculate_separability(internal_weight, external_weight)
     conductance = calculate_conductance(internal_weight, external_weight)
     # the ratio of external to internal conductance
     conductance_ratio = conductance / cohesiveness
     # return in alphabetic order
-    return [community_size, clustering_coefficient, cohesiveness, conductance, conductance_ratio, density, seperability]
-
-
-def check_clustering(A, node):
-    w3 = internal_edges.dot(internal_edges).dot(internal_edges)
-    print w3
-    w_max = np.ones(internal_edges.shape)
-    print w_max
-    denom = internal_edges.dot(w_max).dot(internal_edges)
-    print denom
-    clustering_coefficients = np.divide(np.diag(w3), np.diag(denom))
-    return clustering_coefficients
+    return [community_size, clustering_coefficient, cohesiveness, conductance, conductance_ratio, density, separability]
 
 
 if __name__ == '__main__':
 
-    full_path = 'local_results/community_analysis' + str(datetime.now().strftime("%Y%m%d-%H%M%S")) + '.csv'
+    data = pd.read_csv('local_resources/plos_one_data.csv', index_col=0)
+    data.index.name = 'community'
+    data = data.reset_index()
+
+    full_path = 'results/community_analysis' + str(datetime.now().strftime("%Y%m%d-%H%M%S")) + '.csv'
     header = ['community', 'size', 'clustering_coefficient', 'cohesiveness', 'conductance', 'conductance_ratio',
               'density', 'separability']
     with open(full_path, 'wb') as f:
         writer = csv.writer(f)
         writer.writerow(header)
-    with open('resources/tags_to_plot.csv', 'r') as f:
-        reader = csv.reader(f)
-        for line in reader:
-            tag = line[0]
-            if tag.lower() == 'community':
-                continue  # file has a header
-            elif tag.lower() == 'mixed martial arts':
-                tag = 'taekwondo'
-            elif tag.lower() == 'adult actor':
-                tag = 'pornstar'
-            ids = get_community_ids(tag)
-            print tag, ' has ', len(ids), ' members'
-            results = run_analysis_suite(ids, signatures, star_lookup, generate_graphml=True, tag=tag)
-            print header
-            print 'results are ', results
-            with open(full_path, 'ab') as f:
-                writer = csv.writer(f)
-                output = [tag] + results
-                writer.writerow(output)
-
-    """
-    full_path = './community_analysis.csv'
-    with open(full_path,'a') as f:
-        writer = csv.writer(f)
-        writer.writerow(['community',' size','clustering_coefficient','cohesiveness','conductance','conductance_ratio','density','separability'])
-    
-        athletics_path = 'D:/workspace/science_sandbox/locality_sensitive_hashing/local_resources/ICWSM15/athletics/athletics_handles.csv'
-        tag = 'athletics'
-        ids = get_node_list(athletics_path,0)
-        ids = ids[0:100]
-        results = run_analysis_suite(ids,signatures,False,tag)
-        output = [tag] + results
-        writer.writerow(output)
-        print results
-#    conductance = calculate_conductance(signatures,indices)
-#    print conductance
-
-# test with adidas and nike
-   # jaccs = get_jaccards(signatures,[207,10035])
-    #print a.shape
-    #print a[:,[207,10035]]
-    
-    # test on some fully connected binary networks
-    internal_edges = np.array([[0,1,1,1],[1,0,1,1],[1,1,0,1],[1,1,1,0]],dtype=float)
-    #internal_edges = np.array([[0,1,1,1,1],[1,0,1,1,1],[1,1,0,1,1],[1,1,1,0,1],[1,1,1,1,0]],dtype=float)  
-    #internal_edges = np.array([[0,1,1],[1,0,1],[1,1,0]],dtype=float)
-    n_iterations = 3
-    clustering = calculate_clustering_coefficient(internal_edges)
-    print 'clustering, ', clustering
-    cohesiveness = calculate_cohesiveness(internal_edges,n_iterations,internal_edges.shape[0])
-    print cohesiveness
-      
-    print 'CHEKCING FUNCTION'
-    print check_clustering(internal_edges,1)
-    """
+    grouped = data.groupby('community')
+    for group in grouped:
+        results = run_analysis_suite(group, data, generate_graphml=True)
+        print header
+        print 'results are ', results
+        with open(full_path, 'ab') as f:
+            writer = csv.writer(f)
+            output = [tag] + results
+            writer.writerow(output)
